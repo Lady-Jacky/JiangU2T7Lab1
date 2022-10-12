504a3d1885c058a815b162540518813178175276
Jacky <jackyj12@nycstudents.net>
2022-10-12 14:56:11 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = scan.nextLine();
+        System.out.println("String length: " + str1.length());
+        System.out.println("First half: " + str1.substring(0, str1.length() / 2));
+        System.out.println("Second half: " + str1.substring(str1.length() / 2));
+
+        System.out.print("Enter second string: ");
+        String str2 = scan.nextLine();
+        if (str1.length() > str2.length()) {
+            System.out.println(str1 + " is longer");
+        } else {
+            if (str1.length() < str2.length()) {
+                System.out.println(str2 + " is longer");
+            } else {
+                System.out.println("Both strings have the same length.");
+            }
+        }
+
+        if (str1.equals(str2)) {
+            System.out.println("Both strings have the exact same characters");
+        } else {
+            if (str1.compareTo(str2) > 0) {
+                System.out.println(str1 + " is first alphabetically");
+            } else {
+                if (str1.compareTo(str2) < 0) {
+                    System.out.println(str2 + " is first alphabetically");
+                }
+            }
+        }
+
+        if (str1.indexOf(str2) != -1) {
+            System.out.println(str2 + " is found in " + str1 + " at index " + str1.indexOf(str2));
+        } else {
+            System.out.println(str2 + " is NOT found in " + str1);
+        }
     }
 }
+
+
